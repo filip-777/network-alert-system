9f961a1d9e331d05630ad792418f2d4b0cf49e2d
Filip Sęk <44314861+filip-777@users.noreply.github.com>
2025-05-08 20:20:49 +0200

    Merge pull request #17
    * ADD: TEST class and junit dependency.

---
+++ src/test/java/pl/sek/nas/AlertNetworkTest.java
@@ -0,0 +1,24 @@
+package pl.sek.nas;
+
+import org.junit.jupiter.api.Test;
+
+class AlertNetworkTest {
+
+  @Test
+  void addService() {}
+
+  @Test
+  void addDependency() {}
+
+  @Test
+  void getDependencies() {}
+
+  @Test
+  void findAlertPropagationPath() {}
+
+  @Test
+  void getAffectedServices() {}
+
+  @Test
+  void suggestContainmentEdges() {}
+}
